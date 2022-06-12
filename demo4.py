82d7350f86eb09bdc24971efce33b33d9e847452
violet4y <98242766+violet4y@users.noreply.github.com>
2022-06-12 09:17:21 +0900

    a

--- demo4.py
+++ demo4.py
@@ -6,5 +6,5 @@ mc = Minecraft.create(port=param.PORT_MC)  # MCJE:14712, MCPI:4711
 mc.postToChat("demo4")
 
 mc.postToChat("Hello!")
-mc.postToChat("set goldblock 0 110 0")
+mc.postToChat("set goldblock at 0 110 0")
 mc.setBlock(0, 110, 0, param.GOLD_BLOCK)
