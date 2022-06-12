41aca7a661cfa104ff17f50335b7e4611687cdc1
violet4y <98242766+violet4y@users.noreply.github.com>
2022-06-12 11:16:35 +0900

    gold

--- demo4.py
+++ demo4.py
@@ -5,6 +5,6 @@ import param_MCJE as param
 mc = Minecraft.create(port=param.PORT_MC)  # MCJE:14712, MCPI:4711
 mc.postToChat("demo4")
 
-mc.postToChat("Hello!")
-mc.postToChat("set goldblock at 0 110 0")
-mc.setBlock(0, 110, 0, param.GOLD_BLOCK)
+mc.postToChat("ピラミッド作成")
+mc.postToChat("set goldblock at 10 63 10")
+mc.setBlock(10, 63, 10, param.GOLD_BLOCK)
