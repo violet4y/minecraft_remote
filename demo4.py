25f9535eb06249f9472915bca6cc47ecf9ec2a0b
violet4y <98242766+violet4y@users.noreply.github.com>
2022-06-12 12:26:57 +0900

    a

--- demo4.py
+++ demo4.py
@@ -9,12 +9,21 @@ import param_MCJE as param
 mc = Minecraft.create(port=param.PORT_MC)  # MCJE:14712, MCPI:4711
 mc.postToChat("demo4")
 
+mc.postToChat("clear")
+mc.setBlocks(10, 63, 10, 20, 65, 20, param.AIR)
+sleep(1)
+
 mc.postToChat("ピラミッド作成")
 mc.postToChat("set goldblock at 10 63 10")
 X = 10
 Y = 63
 Z = 10
-mc.setBlock(X, Y, Z, param.GOLD_BLOCK)
-X += 1
-sleep(0.5)
-mc.setBlock(X, Y, Z, param.GOLD_BLOCK)
+POS = 0
+for i in range(7):
+    for i in range(11):
+        mc.setBlocks(X+POS, Y+POS, Z+POS, X+10-POS*3, Y+POS, Z-POS, param.GOLD_BLOCK)
+        X += 1
+        sleep(0.5)
+    POS += 1
+    
+    
