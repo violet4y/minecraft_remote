7f5ccc131298cee44ace765c639bcc210346edf5
violet4y <98242766+violet4y@users.noreply.github.com>
2022-06-23 21:12:32 +0900

    finished

--- demo6.py
+++ demo6.py
@@ -1,16 +1,21 @@
+#階段作成
 from multiprocessing.connection import wait
 from time import sleep
 from mcje.minecraft import Minecraft
 import param_MCJE as param
 mc = Minecraft.create(port=param.PORT_MC)
 
-X = 20
+X = 30
 Y = 63
 Z = 10
 POS = 0
 
-mc.postToChat("階段")
+mc.setBlocks(X, Y, Z, X + 10, Y + 10, Z + 10, param.AIR)
+sleep(1)
+mc.postToChat("start")
 
 for i in range(10):
-    mc.setBlocks(X + POS, Y, Z, param.GOLD_BLOCK)
+    mc.setBlocks(X + POS, Y + POS, Z, X + POS, Y + POS, Z + 10, param.GOLD_BLOCK)
+    sleep(0.3)
+    POS += 1
 mc.postToChat("finished")
