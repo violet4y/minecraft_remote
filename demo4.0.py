a1c01d24ab7f1f862178a51f4c5ef3e4451103fe
violet4y <98242766+violet4y@users.noreply.github.com>
2022-10-09 00:56:33 +0900

    pyramid

---
+++ demo4.0.py
@@ -0,0 +1,30 @@
+# ピラミッド作成プログラム
+from multiprocessing.connection import wait
+from re import X
+from time import sleep
+from tkinter import Y
+from mcje.minecraft import Minecraft
+import param_MCJE as param
+
+mc = Minecraft.create(port=param.PORT_MC)  # MCJE:14712, MCPI:4711
+mc.postToChat("demo4")
+
+X = 10   #X座標
+Y = 63   #Y座標
+Z = 10   #Z座標
+POS = 0   #位置情報 
+
+mc.setBlocks(X, Y, Z, X+10, Y+10, Z+10, param.AIR)
+sleep(3)
+
+mc.postToChat("ピラミッド作成開始")
+
+for i in range(7):
+    for i in range(11-POS*2):
+        mc.setBlocks(X+POS, Y+POS, Z+POS, X+10-POS, Y+POS, Z+POS, param.GOLD_BLOCK)
+        Z += 1
+        sleep(0.8)
+    Z -= 11-POS*2
+    POS += 1
+mc.postToChat("ピラミッド作成完了!!")
+mc.setBlock(X+POS-2, Y+POS-2, Z+POS-3, param.GLOWSTONE)
