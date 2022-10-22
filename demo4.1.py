a52fe9aa94c8de0145012c6b7d925eaa54e26252
violet4y <98242766+violet4y@users.noreply.github.com>
2022-10-23 01:30:49 +0900

    pyramid

--- demo4.1.py
+++ demo4.1.py
@@ -12,5 +12,8 @@ from tkinter import Y
 import demo4
 
 mc = Minecraft.create(port=param.PORT_MC)  # MCJE:14712, MCPI:4711
-mc.postToChat("demo4")
+mc.postToChat("demo4-set pyramid!!")
 
+demo4.set_piramid(X=10, Y=63, Z=10, H=6, SLEEP=0.8, BLOCK=param.GOLD_BLOCK)
+
+mc.postToChat("finish")
