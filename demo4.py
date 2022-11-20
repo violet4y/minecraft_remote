29bde9f8c17a56fb45a6ccfb7e6cf489a3ecde8c
violet4y <98242766+violet4y@users.noreply.github.com>
2022-11-20 11:05:23 +0900

    pyramid

--- demo4.py
+++ demo4.py
@@ -1,38 +1,19 @@
 # ピラミッド作成プログラム
 from mcje.minecraft import Minecraft
 import param_MCJE as param
-from time import sleep
-
 """
 from multiprocessing.connection import wait
 from re import X
+from time import sleep
 from tkinter import Y
 """
-"demo4.1.pyの元"
+"量産型"
+
+import pyramid
 
 mc = Minecraft.create(port=param.PORT_MC)  # MCJE:14712, MCPI:4711
-mc.postToChat("demo4")
+mc.postToChat("demo4-set pyramid!!")
 
-def set_piramid(X=10, Y=63, Z=10, H=6, SLEEP=0.8, BLOCK=param.GOLD_BLOCK):
-    POS = 0   #位置情報 
-    mc.setBlocks(X, Y, Z, X+2*H-2, Y+H+1, Z+2*H-2, param.AIR)
-    sleep(3)
+pyramid.set_piramid(X=10, Y=63, Z=10, H=6, SLEEP=0.8, BLOCK=param.GOLD_BLOCK)
 
-    mc.postToChat("ピラミッド作成開始")
-
-    for i in range(H+1):
-        for i in range(2*H-1-POS*2):
-            mc.setBlocks(X+POS, Y+POS, Z+POS, X+2*H-2-POS, Y+POS, Z+POS, BLOCK)
-            Z += 1
-            sleep(SLEEP)
-        Z -= 2*H-1-POS*2
-        POS += 1
-    
-    mc.setBlock(X+POS-2, Y+POS-2, Z+POS-3, param.GLOWSTONE)
-
-    if __name__ == "__main__":
-        mc = Minecraft.create(port=param.PORT_MC)
-        set_piramid(X, Z=10, Y=63, H=6, SLEEP=0.3, BLOCK=param.GOLD_BLOCK)
-
-
-
+mc.postToChat("finish")
